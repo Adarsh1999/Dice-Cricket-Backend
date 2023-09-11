1bea439a051e79612ba2a7b036f4f28ea8d23e64
Adarsh1999 <adarshguptamaurya@gmail.com>
2023-09-10 21:29:37 -0400

    improved code of index.js and solved bugs

--- index.js
+++ index.js
@@ -1,136 +1,97 @@
 import express from "express";
-const app = express();
 import mongoose from "mongoose";
-//always use .js in the end of the file extension as in bcoz of es6
+import cors from "cors"; // Import the cors middleware
 import diceGame from "./dbGame.js";
-// app.use(cors({
-//   origin: true,
-//   credentials: true
-// }));
 import playerNames from "./team.js";
+
+const app = express();
+
+// Use the cors middleware
+app.use(cors({
+  origin: "http://localhost:3000", // Adjust this to your frontend URL
+  credentials: true,
+}));
 
 app.use(express.json());
 
-// https://stackoverflow.com/questions/16810449/when-to-use-next-and-return-next-in-node-js
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "*");
   next();
 });
 
-//database connection
+// Database connection
+const connection_url = "mongodb+srv://admin:pass%40word@cluster0.caap5.mongodb.net/dice-cricket?retryWrites=true&w=majority";
 
-const connection_url =""
 mongoose.connect(connection_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
 });
-
 const con = mongoose.connection;
 
 con.on("open", () => {
-  console.log("connected....");
+  console.log("Connected to MongoDB...");
 });
 
 let response = {};
-app.get("/teams", (req, res) => {
-  let q = req.query.q;
-  let p = req.query.p;
-  // if (q === "Australia") {
-  //   response={
-  //     "team1":playerNames.Australia,
-  //   }
-  console.log(p, q);
-  q === "Australia"
-    ? (response = {
-        team1: playerNames.Australia,
-      })
-    : q === "New Zealand"
-    ? (response = {
-        team1: playerNames.New_Zealand,
-      })
-    : q === "England"
-    ? (response = {
-        team1: playerNames.England,
-      })
-    : q === "India"
-    ? (response = {
-        team1: playerNames.India,
-      })
-    : console.log("no team");
 
-  if (p === "England") {
-    response = {
-      ...response,
-      team2: playerNames.England,
-    };
-    res.json(response);
-  } else if (p === "Australia") {
-    response = {
-      ...response,
-      team2: playerNames.Australia,
-    };
-    res.json(response);
-  } else if (p === "New Zealand") {
-    response = {
-      ...response,
-      team2: playerNames.New_Zealand,
-    };
-    res.json(response);
-  } else if (p === "India") {
-    response = {
-      ...response,
-      team2: playerNames.India,
-    };
-    res.json(response);
-  } else {
-    res.send("2nd team not selected correctly");
+app.get("/teams", (req, res) => {
+  const { q, p } = req.query;
+  const teams = ["Australia", "New Zealand", "England", "India"];
+  
+  if (!teams.includes(q) || !teams.includes(p)) {
+    return res.status(400).send("One or both teams not selected correctly");
+  }
+
+  const response = {
+    team1: playerNames[q],
+    team2: playerNames[p]
+  };
+
+  res.json(response);
+});
+
+
+app.get("/history/all", async (req, res) => {
+  try {
+    const data = await diceGame.find({});
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send(err);
   }
 });
-app.get("/history/all", (req, res) => {
+
+app.post("/history/new", async (req, res) => {
   const dbMessage = req.body;
-  diceGame.find(dbMessage, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(data);
-    }
-  });
+  try {
+    const data = await diceGame.create(dbMessage);
+    res.status(201).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-app.post("/history/new", (req, res) => {
-  const dbMessage = req.body;
-  diceGame.create(dbMessage, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(201).send(data);
-    }
-  });
+app.get("/history/delete", async (req, res) => {
+  try {
+    const data = await diceGame.deleteMany({});
+    res.json(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
-app.get("/history/delete", (req, res) => {
-  diceGame.remove({}, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.json(data);
-    }
-  });
-});
-
-app.post(`/history/find`, (req, res) => {
+app.post(`/history/find`, async (req, res) => {
   const { id } = req.body;
 
-  diceGame.find({ _id: id }, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(data);
-    }
-  });
+  try {
+    const data = await diceGame.find({ _id: id });
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
+
+
 const port = process.env.PORT || 3001;
 
 app.listen(port, () =>
